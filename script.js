9f8011dae26403391ac9169af3f4a05f7457070f
Sebastian Hoyos <sebastianhoyos145@gmail.com>
2025-02-15 11:37:39 -0500

    wip

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    loop:true,
-    autoplay:true,
-    speed:3000
-  });
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  speed: 3000,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
 
-  const menu = document.querySelector('.menu')
-  const toggle = document.querySelector('#toggle')
+const menu = document.querySelector('.menu')
+const toggle = document.querySelector('#toggle')
 
-  toggle.addEventListener('click',function(){
-    menu.classList.toggle('active');
-  })
+toggle.addEventListener('click', function () {
+  const isExpanded = toggle.getAttribute('aria-expanded') === 'true';
+  toggle.setAttribute('aria-expanded', !isExpanded);
+  menu.classList.toggle('active');
+});
